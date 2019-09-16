b178369d2e5ce196313a3d6130bd8513bc9c7433
Penguin <besedinalexey@gmail.com>
2019-09-16 10:30:00 +0300

    Lab1 the rest

--- Lab1/Unit1.cpp
+++ Lab1/Unit1.cpp
@@ -14,3 +14,23 @@ __fastcall TForm1::TForm1(TComponent* Owner)
 {
 }
 //---------------------------------------------------------------------------
+void __fastcall TForm1::Button1Click(TObject *Sender)
+{
+	Image1->Visible = !Image1->Visible;
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::Button2Click(TObject *Sender)
+{
+	Image2->Visible = !Image2->Visible;
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::Button3Click(TObject *Sender)
+{
+		 Image3->Visible = !Image3->Visible;
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::Button4Click(TObject *Sender)
+{
+Image4->Visible = !Image4->Visible;
+}
+//---------------------------------------------------------------------------
