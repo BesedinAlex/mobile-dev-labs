9a3c4caca446bf9ce18dbbf7af238da2ded25eee
Alexey <besedinalexey@gmail.com>
2019-10-23 13:42:37 +0300

    LabMemTable some changes

--- LabMemTable/LabMemTable.cpp
+++ LabMemTable/LabMemTable.cpp
@@ -14,3 +14,83 @@ __fastcall TForm1::TForm1(TComponent* Owner)
 {
 }
 //---------------------------------------------------------------------------
+void __fastcall TForm1::Button1Click(TObject *Sender)
+{
+    tb->Post();
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::Button2Click(TObject *Sender)
+{
+    tb->Cancel();
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::ButtonFilterOnClick(TObject *Sender)
+{
+    tb->Filter = EditFilter->Text;
+    tb->Filtered = true;
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::ButtonFilterOffClick(TObject *Sender)
+{
+    tb->Filtered = false;
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::ButtonSaveFileClick(TObject *Sender)
+{
+	tb->SaveToFile(Ioutils::TPath::GetDocumentsPath() + PathDelim + EditFileName->Text + ".json");
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::ButtonLoadFileClick(TObject *Sender)
+{
+	tb->LoadFromFile(Ioutils::TPath::GetDocumentsPath() + PathDelim + EditFileName->Text + ".json");
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::ButtonAddAgeClick(TObject *Sender)
+{
+	tb->Edit();
+	tbAge->Value += 1;
+	tb->Post();
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::ButtonAddMoneyClick(TObject *Sender)
+{
+	tb->First();
+	while (!tb->Eof) {
+		tb->Edit();
+		tbMoney->Value += 100;
+		tb->Post();
+        tb->Next();
+	}
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::CheckBoxReadOnlyGridChange(TObject *Sender)
+{
+	Grid1->ReadOnly = CheckBoxReadOnlyGrid->IsChecked;
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::CheckBoxReadOnlyTableChange(TObject *Sender)
+{
+	tb->ReadOnly = CheckBoxReadOnlyTable->IsChecked;
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::ButtonLocateClick(TObject *Sender)
+{
+	tb->Locate(tbAge->FieldName, EditL->Text);
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::ButtonLookupClick(TObject *Sender)
+{
+    ShowMessage(tb->Lookup(tbAge->FieldName, EditL->Text, tbFIO->FieldName));
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::ButtonSumClick(TObject *Sender)
+{
+	double x = 0;
+	tb->First();
+	while (!tb->Eof) {
+		x += tbMoney->Value;
+		tb->Next();
+	}
+	ShowMessage(FloatToStr(x));
+}
+//---------------------------------------------------------------------------
--- LabMemTable/LabMemTable.h
+++ LabMemTable/LabMemTable.h
@@ -6,10 +6,90 @@
 #include <System.Classes.hpp>
 #include <FMX.Controls.hpp>
 #include <FMX.Forms.hpp>
+#include <Data.Bind.Components.hpp>
+#include <Data.Bind.DBScope.hpp>
+#include <Data.Bind.EngExt.hpp>
+#include <Data.Bind.Grid.hpp>
+#include <Data.DB.hpp>
+#include <FireDAC.Comp.Client.hpp>
+#include <FireDAC.Comp.DataSet.hpp>
+#include <FireDAC.DApt.Intf.hpp>
+#include <FireDAC.DatS.hpp>
+#include <FireDAC.Phys.Intf.hpp>
+#include <FireDAC.Stan.Error.hpp>
+#include <FireDAC.Stan.Intf.hpp>
+#include <FireDAC.Stan.Option.hpp>
+#include <FireDAC.Stan.Param.hpp>
+#include <FireDAC.Stan.StorageBin.hpp>
+#include <Fmx.Bind.DBEngExt.hpp>
+#include <Fmx.Bind.Editors.hpp>
+#include <Fmx.Bind.Grid.hpp>
+#include <FMX.Controls.Presentation.hpp>
+#include <FMX.Grid.hpp>
+#include <FMX.Grid.Style.hpp>
+#include <FMX.ScrollBox.hpp>
+#include <FMX.Types.hpp>
+#include <System.Bindings.Outputs.hpp>
+#include <System.Rtti.hpp>
+#include <System.IOUtils.hpp>
+#include <Data.Bind.Controls.hpp>
+#include <Fmx.Bind.Navigator.hpp>
+#include <FMX.Edit.hpp>
+#include <FMX.Layouts.hpp>
+#include <FMX.StdCtrls.hpp>
+#include <FireDAC.Stan.StorageJSON.hpp>
 //---------------------------------------------------------------------------
 class TForm1 : public TForm
 {
 __published:	// IDE-managed Components
+	TFDMemTable *tb;
+	TStringField *tbFIO;
+	TStringField *tbTel;
+	TIntegerField *tbAge;
+	TCurrencyField *tbMoney;
+	TGrid *Grid1;
+	TBindSourceDB *BindSourceDB1;
+	TBindingsList *BindingsList1;
+	TLinkGridToDataSource *LinkGridToDataSourceBindSourceDB1;
+	TBindNavigator *NavigatorBindSourceDB1;
+	TEdit *Edit1;
+	TEdit *Edit2;
+	TEdit *Edit3;
+	TEdit *Edit4;
+	TLinkControlToField *LinkControlToField1;
+	TLinkControlToField *LinkControlToField2;
+	TLinkControlToField *LinkControlToField3;
+	TLinkControlToField *LinkControlToField4;
+	TButton *Button1;
+	TButton *Button2;
+	TEdit *EditFilter;
+	TButton *ButtonFilterOn;
+	TButton *ButtonFilterOff;
+	TEdit *EditFileName;
+	TButton *ButtonSaveFile;
+	TButton *ButtonLoadFile;
+	TFDStanStorageJSONLink *FDStanStorageJSONLink1;
+	TButton *ButtonAddMoney;
+	TButton *ButtonAddAge;
+	TCheckBox *CheckBoxReadOnlyGrid;
+	TCheckBox *CheckBoxReadOnlyTable;
+	TButton *ButtonLocate;
+	TButton *ButtonLookup;
+	TEdit *EditL;
+	TButton *ButtonSum;
+	void __fastcall Button1Click(TObject *Sender);
+	void __fastcall Button2Click(TObject *Sender);
+	void __fastcall ButtonFilterOnClick(TObject *Sender);
+	void __fastcall ButtonFilterOffClick(TObject *Sender);
+	void __fastcall ButtonSaveFileClick(TObject *Sender);
+	void __fastcall ButtonLoadFileClick(TObject *Sender);
+	void __fastcall ButtonAddAgeClick(TObject *Sender);
+	void __fastcall ButtonAddMoneyClick(TObject *Sender);
+	void __fastcall CheckBoxReadOnlyGridChange(TObject *Sender);
+	void __fastcall CheckBoxReadOnlyTableChange(TObject *Sender);
+	void __fastcall ButtonLocateClick(TObject *Sender);
+	void __fastcall ButtonLookupClick(TObject *Sender);
+	void __fastcall ButtonSumClick(TObject *Sender);
 private:	// User declarations
 public:		// User declarations
 	__fastcall TForm1(TComponent* Owner);
