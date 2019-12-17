aa6db69d37feacdef4ad911e8eae67d324434098
Alexey <besedinalexey@gmail.com>
2019-12-17 15:58:01 +0300

    LabEffects

---
+++ LabEffects/Unit1.h
@@ -0,0 +1,34 @@
+//---------------------------------------------------------------------------
+
+#ifndef Unit1H
+#define Unit1H
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <FMX.Controls.hpp>
+#include <FMX.Forms.hpp>
+#include <FMX.Controls.Presentation.hpp>
+#include <FMX.Effects.hpp>
+#include <FMX.Objects.hpp>
+#include <FMX.StdCtrls.hpp>
+#include <FMX.Types.hpp>
+#include <FMX.Filter.Effects.hpp>
+//---------------------------------------------------------------------------
+class Tfm : public TForm
+{
+__published:	// IDE-managed Components
+	TImage *Image1;
+	TImage *Image2;
+	TImage *Image3;
+	TImage *Image4;
+	TGlowEffect *GlowEffect1;
+	TShadowEffect *ShadowEffect1;
+	TPinchEffect *PinchEffect1;
+	TSwirlEffect *SwirlEffect1;
+private:	// User declarations
+public:		// User declarations
+	__fastcall Tfm(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE Tfm *fm;
+//---------------------------------------------------------------------------
+#endif
