e8db140d58a744281533b707665ad38d2cdb8661
Alexey <besedinalexey@gmail.com>
2019-12-23 22:34:38 +0300

    Removed labs

--- LabCountBoxes/uUtils.h
+++
@@ -1,8 +0,0 @@
-//---------------------------------------------------------------------------
-
-#ifndef uUtilsH
-#define uUtilsH
-#include <FMX.Controls.Presentation.hpp>
-//---------------------------------------------------------------------------
-int* RandomArrayUnique(int aMax, int aCount);
-#endif
--- LabGenPassword/uUtils.h
+++
@@ -1,8 +0,0 @@
-//---------------------------------------------------------------------------
-
-#ifndef uUtilsH
-#define uUtilsH
-#include <System.Classes.hpp>
-//---------------------------------------------------------------------------
-UnicodeString RandomStr(int aLenght, bool aLower, bool aUpper, bool aNumber, bool aSpecl);
-#endif
